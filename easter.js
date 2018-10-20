0062d885bfa44038d2203f7c9b18d41a63de7242
richlove45 <richlovenkansah@gmail.com>
2018-10-20 15:41:07 -0400

    ish ya boi nomad

--- easter.js
+++ easter.js
@@ -1,5 +1,4 @@
 // import readline-sync module, don't touch!
-let y = 1947
 let a = y % 19 
 let b = y / 100 
 let c = y % 100 
@@ -14,4 +13,4 @@ let r = 32 + 2 * e + 2 * i - h - k % 7
 let m = a + 11 * h + 22 * r / 451 
 let n = h + r - 7 * m + 114 / 31 
 let p = h + r - 7 * m + 114 % 31 
-console.log ()
+console.log ()  
