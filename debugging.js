ee9ffa321ee73b7d14098b0e58454cd71f41fc20
richlove45 <richlovenkansah@gmail.com>
2018-11-03 13:56:09 -0400

    Debug Day

--- debugging.js
+++ debugging.js
@@ -1,28 +1,29 @@
 // Remove block comments when ready to debug a program.
-
+const READLINE = require("readline-sync")
 // Program #1: "Careless"
-/*
-let x == 3;
-if(x => 3 {
-  X+++;
-}} Else {
-  X =+ 2;
 
-console.log = X;
-*/
-
-// Program #2: "Bad logic and math"
-/*
-let money = 28;
-if(money > 50 && money <= 20) {
-  money /= 2;
-} else {
-  money /= 0;
+let x = 3;
+if(x => 3) {
+  (x++);
+}else{
+  x =+ 2;
 }
-*/
+console.log(x);
+
+
+
+ //Program #2: "Bad logic and math"
+
+let money = 28;
+if(money < 50 && money <= 20) {
+  money = 2;
+} else {
+  money = 0;
+}
+console.log("money")
 
 // Program #3: "And what else?"
-/*
+
 let num1 = READLINE.question("Enter num1: ");
 let num2 = READLINE.question("Enter num2: ");
 let num3 = READLINE.question("Enter num3: ");
@@ -35,4 +36,4 @@ else if(num2 > num3) {
 else {
   console.log(`${num3} is the largest value.`);
 }
-*/
+
